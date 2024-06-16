5f0e212f0d6287c0b35e8d17ef85c62cff8f3706
mujakkir-maher <mujakkirahmad78@gmail.com>
2024-06-16 17:18:32 +0600

    initial-commit

---
+++ ptint_system.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 59;
+    float b = 56.7;
+    char c = 'A';
+    printf("the int number is %d\n",a);
+    printf("the float number is %f\n",b);
+    printf("the char is %c\n",c);
+    return 0;
+}
---
+++ variable.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a = 70;
+    int b = 50;
+    int x = a+b;
+    printf("%d",x);
+    return 0;
+}
